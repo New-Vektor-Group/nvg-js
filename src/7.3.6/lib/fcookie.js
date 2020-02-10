bf0d0843067b8aab14ab8ec9872a4c478d2f9695
Sasha <40319171+lonagi@users.noreply.github.com>
2020-02-10 22:29:55 +0200

    some fix

--- src/7.3.6/lib/fcookie.js
+++ src/7.3.6/lib/fcookie.js
@@ -1,51 +1,51 @@
-function nvg_cook(inputId)
-{
-	nvg_cook.prototype.delMe = function()
-	{
-		localStorage.removeItem(this.inputId.slice(1));
-	}
-
-  nvg_cook.getCookie(name) {
+nvg_cook.getCookie(name) {
   var matches = document.cookie.match(new RegExp(
     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
   ));
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
-  nvg_cook.setCookie(name, value, options) {
+nvg_cook.setCookie(name, value, options) {
   options = options || {};
 
-  var expires = options.expires;
+var expires = options.expires;
 
-  if (typeof expires == "number" && expires) {
-    var d = new Date();
-    d.setTime(d.getTime() + expires * 1000);
-    expires = options.expires = d;
-  }
-  if (expires && expires.toUTCString) {
-    options.expires = expires.toUTCString();
-  }
-
-  value = encodeURIComponent(value);
-
-  var updatedCookie = name + "=" + value;
-
-  for (var propName in options) {
-    updatedCookie += "; " + propName;
-    var propValue = options[propName];
-    if (propValue !== true) {
-      updatedCookie += "=" + propValue;
-    }
-  }
-
-  document.cookie = updatedCookie;
+if (typeof expires == "number" && expires) {
+  var d = new Date();
+  d.setTime(d.getTime() + expires * 1000);
+  expires = options.expires = d;
+}
+if (expires && expires.toUTCString) {
+  options.expires = expires.toUTCString();
 }
 
-  nvg_cook.deleteCookie(name) {
-  nvg_cook.setCookie(name, "", {
-    expires: -1
-  })
+value = encodeURIComponent(value);
+
+var updatedCookie = name + "=" + value;
+
+for (var propName in options) {
+  updatedCookie += "; " + propName;
+  var propValue = options[propName];
+  if (propValue !== true) {
+    updatedCookie += "=" + propValue;
+  }
 }
+
+document.cookie = updatedCookie;
+}
+
+nvg_cook.deleteCookie(name) {
+nvg_cook.setCookie(name, "", {
+  expires: -1
+});
+}
+
+function nvg_cook(inputId)
+{
+	nvg_cook.prototype.delMe = function()
+	{
+		localStorage.removeItem(this.inputId.slice(1));
+	}
 
   this.inputId = inputId;
 
