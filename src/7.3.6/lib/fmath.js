4e9d30bb8413ea3ad98a4a025ed37ba184ffeece
Sasha <40319171+lonagi@users.noreply.github.com>
2020-02-16 19:53:53 +0200

    Math ready

--- src/7.3.6/lib/fmath.js
+++ src/7.3.6/lib/fmath.js
@@ -8,9 +8,9 @@ nvgR.I = function(min, max, f)
  		return Math.floor(Math.random() * (max - min) + min);
  	else
  		return Math.random() * (max - min) + min;
-}
+};
 
-nvgR.C = function(caps = true, lang = 'en')
+nvgR.C = function(caps, lang)
 {
 	if(caps === undefined)
 		caps = true;
@@ -31,7 +31,7 @@ nvgR.C = function(caps = true, lang = 'en')
 	var rnum = Math.floor(Math.random() * chars.length);
 	var randomstring = chars.substring(rnum,rnum+1);
 	return randomstring;
-}
+};
 
 nvgR.D = function(zero)
 {
@@ -42,19 +42,19 @@ nvgR.D = function(zero)
 		return nvgR.I(0,10,1);
 	else
 		return nvgR.I(1,10,1);
-}
+};
 
 nvgR.N = function(d)
 {
 	if(d === undefined)
 		d = 1;
 	return nvgR.I(Math.pow(10,d-1),Math.pow(10,d-1)*10,1);
-}
+};
 
 function nvgR(){}
 
 nvgM.sigm = function(x) 
 {
  	return 1/(1+Math.pow(Math.E, -x));
-}
+};
 function nvgM(){}
