a413c8724a627bb1ac5706589f5c3f4207c5d0fb
Sasha <40319171+lonagi@users.noreply.github.com>
2020-02-16 19:59:21 +0200

    Text ready

--- src/7.3.6/lib/ftext.js
+++ src/7.3.6/lib/ftext.js
@@ -1,24 +1,23 @@
-function nvgtext()
+nvgtext.nl2br = function(str, is_xhtml)
 {
-	nvgtext.nl2br = function(str, is_xhtml) {
-    if (typeof str === 'undefined' || str === null) {
-        return '';
-    }
-    var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
-    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
-	}
+	if (typeof str === 'undefined' || str === null)
+	    return '';
+	var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+	return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+};
 
-	nvgtext.nocopy = function(element)
-	{
-		$(element).css("-webkit-user-select","none");
-		$(element).css("-khtml-user-select","none");
-		$(element).css("-moz-user-select","none");
-		$(element).css("-ms-user-select","none");
-		$(element).css("-o-user-select","none");
-		$(element).css("user-select","none");
-		
-		$(element).bind('cut copy paste', function(e) {
-			e.preventDefault();
-		});
-	}
-}
+nvgtext.nocopy = function(element)
+{
+	$(element).css("-webkit-user-select","none");
+	$(element).css("-khtml-user-select","none");
+	$(element).css("-moz-user-select","none");
+	$(element).css("-ms-user-select","none");
+	$(element).css("-o-user-select","none");
+	$(element).css("user-select","none");
+	
+	$(element).bind('cut copy paste', function(e) {
+		e.preventDefault();
+	});
+};
+
+function nvgtext(){}
