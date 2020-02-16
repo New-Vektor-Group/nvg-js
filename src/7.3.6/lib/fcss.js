5a0c5a59475e3bda629bbdcfee537e85dd66ec37
Sasha <40319171+lonagi@users.noreply.github.com>
2020-02-16 15:07:04 +0200

    fix bug with appearance

--- src/7.3.6/lib/fcss.js
+++ src/7.3.6/lib/fcss.js
@@ -23,69 +23,76 @@ function nvgs(obj2, scroll_when2, direction2, scroll_when2_2)
 {
   nvgs.sh5df543 = function(objj)
   {
+    $(objj).css("display",'inherit');
     $(objj).stop();
     $(objj).animate({
       opacity: 1
     },150);
-    setTimeout(function(){$(objj).css("display",'inherit')},200);
-  /*$(objj).addClass("show");*/
-  /*$(objj).removeClass("hide");*/
-  /*setTimeout(function(){$(objj).css("transform","translate(0px,0px)");},0);*/
+    /*setTimeout(function(){},200);*/
   }
 
-  nvgs.hds5df543 = function(objj, needshf)
+  nvgs.hds5df543 = function(objj)
   {
     $(objj).stop();
     $(objj).animate({
       opacity: 0
     },150);
-    setTimeout(function(){$(objj).css("display",'none')},200);
- /*$(objj).removeClass("show");*/
- /*$(objj).addClass("hide");*/
- /*setTimeout(function(){if(needshf) $(objj).css("transform","translate(1000px,-10000px)");},0);*/
-}
+    setTimeout(function(){$(objj).css("display",'none')},155);
+  }
 
   nvgs.elsoft = function(element)
   {
     var scroll = $(window).scrollTop();
 
-    if(element.direction==1)
+    if(element.direction == 1)
     {
-      if(scroll>=element.scroll_when)
+      if(scroll >= element.scroll_when)
       {
-        this.needshf = false;
-        nvgs.sh5df543(element.obj);
+        if(this.needshf) {
+          nvgs.sh5df543(element.obj);
+          this.needshf = false;
+        }
       }
       else
       {
-        this.needshf = true;
-        nvgs.hds5df543(element.obj, this.needshf);
+        if(!this.needshf) {
+          nvgs.hds5df543(element.obj);
+          this.needshf = true;
+        }
       }
     }
     else if(element.direction==0)
     {
       if(scroll<=element.scroll_when)
       {
-        this.needshf = false;
-        nvgs.sh5df543(element.obj);
+        if(this.needshf) {
+          nvgs.sh5df543(element.obj);
+          this.needshf = false;
+        }
       }
       else
       {
-        this.needshf = true;
-        nvgs.hds5df543(element.obj, this.needshf);
+        if(!this.needshf) {
+          nvgs.hds5df543(element.obj);
+          this.needshf = true;
+        }
       }
     }
     else
     {
       if(scroll >= element.scroll_when && scroll <= element.scroll_when2)
       {
-        this.needshf = false;
-        nvgs.sh5df543(element.obj);
+        if(this.needshf) {
+          nvgs.sh5df543(element.obj);
+          this.needshf = false;
+        }
       }
       else
       {
-        this.needshf = true;
-        nvgs.hds5df543(element.obj, this.needshf);
+        if(!this.needshf) {
+          nvgs.hds5df543(element.obj);
+          this.needshf = true;
+        }
       }
     }
   }
@@ -111,8 +118,6 @@ function nvgs(obj2, scroll_when2, direction2, scroll_when2_2)
   this.needshf = false;
   nvgs.elsoft(this);
   nvgs.countEls.push(this);
-  /*$(this.obj).addClass("fade");*/
-
 }
 
 $(window).scroll(function ()
