2f610f135ad48939f8aa95f9a384d83657db7cf7
Sasha <lonagi22@gmail.com>
2020-06-21 00:07:39 +0300

    Update ftext.js

--- src/7.6.9/lib/ftext.js
+++ src/7.6.9/lib/ftext.js
@@ -20,4 +20,91 @@ nvgtext.nocopy = function(element)
 	});
 };
 
+nvgtext.boldFunc=function(str, p1, offset, s){
+	return '<strong>'+(p1)+'</strong>'
+};
+
+nvgtext.italicFunc=function(str, p1, offset, s){
+	return '<em>'+(p1)+'</em>'
+};
+
+nvgtext.underlinedFunc=function(str, p1, offset, s){
+	return '<ins>'+(p1)+'</ins>'
+};
+
+nvgtext.ahrefFunc=function(str,b,c,d){
+	return '<a href="'+b+'" target="_blank" class="text-primary">'+(c)+'</a>'
+};
+
+nvgtext.imghrefFunc=function(str,b,c,d){
+	return '<img src="'+b+'" class="img-fluid" style="max-height:500px;">'
+};
+
+nvgtext.hightFunc=function(str, p1, offset, s){
+	return '<span class="bg-primary">'+(p1)+'</span>'
+};
+
+pasBB = function(wh,wha)
+{
+    $textarea=$(wh).parent().next().children("textarea");
+    start = $textarea[0].selectionStart;
+    finish = $textarea[0].selectionEnd;
+    sel = $textarea.get(0).value.substring(start, finish);
+    v = $textarea.val();
+    textBefore = v.substring(0,  start);
+    textAfter  = v.substring(finish, v.length);
+    $textarea.val(textBefore+textAfter);
+    if(sel=="")
+        sel="text";
+    a=" [b]"+sel+"[/b] ";
+    if(wha==1)
+        a=" [i]"+sel+"[/i] ";
+    else if(wha==2)
+        a=" [u]"+sel+"[/u] ";
+    else if(wha==3)
+        a=" [url="+sel+"]"+sel+"[/url] ";
+    else if(wha==4)
+        a=" [h]"+sel+"[/h] ";
+    else if(wha==5)
+        a=" [img="+sel+"]";
+
+    v = $textarea.val();
+    textBefore = v.substring(0,  start);
+    textAfter  = v.substring(start, v.length);
+    $textarea.val(textBefore + a + textAfter);
+
+    $str=BBconvert("#"+$textarea.attr("id"));
+    $textarea.parent().next().next().children(".output-test").html($str);
+};
+
+BBconvert = function(t)
+{
+    $str=nvgtext.nl2br($(t).val());
+    $format_search =  [
+        /\[b\](.*?)\[\/b\]/ig,
+        /\[i\](.*?)\[\/i\]/ig,
+        /\[u\](.*?)\[\/u\]/ig,
+        /\[h\](.*?)\[\/h\]/ig,
+        /\[url=(.*)\](.*)\[\/url\]/ig,
+        /\[img=(.*)\]/ig
+    ];
+    $format_replace = [
+        boldFunc,
+        italicFunc,
+        underlinedFunc,
+        hightFunc,
+        ahrefFunc,
+        imghrefFunc
+    ];
+    for (var i =0;i<$format_search.length;i++) {
+      $str = $str.replace($format_search[i], $format_replace[i]);
+    }
+    return $str;
+};
+
+$(".ianswer").on("input",function(){
+    $str=BBconvert(this);
+    $(this).parent().next().next().children(".output-test").html($str);
+});
+
 function nvgtext(){}
